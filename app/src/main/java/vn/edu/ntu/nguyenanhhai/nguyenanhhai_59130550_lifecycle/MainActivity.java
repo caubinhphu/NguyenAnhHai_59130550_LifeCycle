7b21676a9b631267b4e4a8a6c18fac27a39a223f
Hai tran do <10a3nguyenanhhai@gmail.com>
2020-04-18 00:15:01 +0700

    cau 2 (Toast)

--- app/src/main/java/vn/edu/ntu/nguyenanhhai/nguyenanhhai_59130550_lifecycle/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/nguyenanhhai/nguyenanhhai_59130550_lifecycle/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,41 +13,48 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     Log.d("LifeCycle", "onCreate");
+    Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
   }
 
   @Override
   protected void onStart() {
     super.onStart();
     Log.d("LifeCycle", "onStart");
+    Toast.makeText(getApplicationContext(), "onStart", Toast.LENGTH_SHORT).show();
   }
 
   @Override
   protected void onResume() {
     super.onResume();
     Log.d("LifeCycle", "onResume");
+    Toast.makeText(getApplicationContext(), "onResume", Toast.LENGTH_SHORT).show();
   }
 
   @Override
   protected void onPause() {
     super.onPause();
     Log.d("LifeCycle", "onPause");
+    Toast.makeText(getApplicationContext(), "onPause", Toast.LENGTH_SHORT).show();
   }
 
   @Override
   protected void onStop() {
     super.onStop();
     Log.d("LifeCycle", "onStop");
+    Toast.makeText(getApplicationContext(), "onStop", Toast.LENGTH_SHORT).show();
   }
 
   @Override
   protected void onRestart() {
     super.onRestart();
     Log.d("LifeCycle", "onRestart");
+    Toast.makeText(getApplicationContext(), "onRestart", Toast.LENGTH_SHORT).show();
   }
 
   @Override
   protected void onDestroy() {
     super.onDestroy();
     Log.d("LifeCycle", "onDestroy");
+    Toast.makeText(getApplicationContext(), "onDestroy", Toast.LENGTH_SHORT).show();
   }
 }
